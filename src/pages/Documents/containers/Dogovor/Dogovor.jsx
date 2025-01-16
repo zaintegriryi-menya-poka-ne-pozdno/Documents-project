564b78ed9919496da196108a83a069fadf61868e
Dmitry <leedmitry8091@gmail.com>
2025-01-16 14:16:42 +1100

    dogovor

--- src/pages/Documents/containers/Dogovor/Dogovor.jsx
+++ src/pages/Documents/containers/Dogovor/Dogovor.jsx
@@ -18,33 +18,16 @@ import {
 import CustomFields from '../CustomFields/CustomFields';
 import FilesContainer from '../FilesContainer/FilesContainer';
 import CustomButton from '../../../../components/CustomButton/CustomButton'
+import {banks} from '../banks'
 
 
 const DocsLabels = {
   dogovor: {
     name: 'Договор',
     fields: [
-      { nameId: 'tip_dostavki',   id: 133147, typeValue: 'select',    name: 'Тип доставки' }, 
-      { nameId: 'vid_transporta', id: 133151, typeValue: 'select',    name: 'Вид транспорта' },
-      { nameId: 'vid_oplati',     id: 133373, typeValue: 'select',    name: 'Форма оплаты' }, 
-      { nameId: 'gde_oplata',     id: 179095, typeValue: 'select',    name: 'Оплата в' }, // форма оплаты
-      
-      { nameId: 'dostavka_do',    id: 133153, typeValue: 'select',    name: 'Адрес/дверь' },
-
-      { nameId: 'otkuda',         id: 179029, typeValue: 'select',    name: 'Откуда' },
-      { nameId: 'otkuda_tochno',  id: 1275901, typeValue: 'textarea',  name: 'Откуда точно' },
-      
-      { nameId: 'kuda',           id: 179035, typeValue: 'select',    name: 'Куда' },
-      { nameId: 'kuda_tochno',    id: 179037, typeValue: 'textarea',  name: 'Куда точно' },
-
-      { nameId: 'adress_dostavki',id: 1586685, typeValue: 'text',      name: 'Адрес доставки' },
-
-      { nameId: 'harakter_gruza', id: 1584461, typeValue: 'select',    name: 'Характер груза' },
-      { nameId: 'vid_upakovki',   id: 179501, typeValue: 'textarea',  name: 'Вид упаковки' },
-      { nameId: 'kolvo_mest',     id: 179505, typeValue: 'number',    name: 'Кол-во мест' }, // максимум 28 может быть число (от 1 до 28)
-    
-      { nameId: 'ves_gruza',      id: 133421, typeValue: 'number',    name: 'Вес груза' },
-      { nameId: 'obem_gruza',     id: 133433, typeValue: 'number',    name: 'Объём  груза' },
+      { nameId: 'bank',  id: 1277125, typeValue: 'select',  name: 'Банк' },
+      { nameId: 'number_and_year',  id: 1277129, typeValue: 'text',  name: 'День/Год' },
+      { nameId: 'post_adress',  id: 1277127, typeValue: 'text',  name: 'Почтовый адрес' },
     ],
     typeDocs: 'dogovor',
     createDocs: DocsAPI.createDogovor,
@@ -52,10 +35,21 @@ const DocsLabels = {
   }
 };
 
+const updateInputValue = (name, newValue) => {
+  const element = document.querySelector(`[data-id="${name}"]`);
+  if (element) {
+    const input = element.querySelector('input');
+    if (input) {
+      input.value = newValue;
+      input.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+  }
+};
+
+
 const customFields = [
-  { name: 'Отправитель',  entity: 'sender'    },
-  { name: 'Получатель',   entity: 'recipient' },
-  { name: 'Плательщик',   entity: 'payer'     }
+  { name: 'Клиент',  entity: 'company'    },
+  { name: 'Водитель',   entity: 'contact' }
 ];
 
 
@@ -64,27 +58,26 @@ const DogovorContainer = ({ type }) => {
   const [fieldValues, setFieldValues] = useState({});
   const [dataDocs, setDataDocs] = useState(null);
   const { name, fields } = DocsLabels[type];
+  const [documents, setDocuments] = useState([])
 
   const [summTotal, setSummTotal] = useState(0);
 
   const [isLoading, setIsLoading] = useState(false); // новое состояние
 
   const [entities, setEntities] = useState({
-    sender: {},
-    recipient: {},
-    payer: {},
+    contact: {},
+    company: {},
   });
 
   const extractEntities = () => {
     const newEntities = {
-      sender: {},
-      recipient: {},
-      payer: {},
+      contact: {},
+      company: {},
     };
 
-    document.querySelectorAll('.entity__sender, .entity__recipient, .entity__payer').forEach(entity => {
-      const entityClass = entity.classList.contains('entity__sender') ? 'sender' :
-        entity.classList.contains('entity__recipient') ? 'recipient' : 'payer';
+    document.querySelectorAll('.entity__company, .entity__contact').forEach(entity => {
+      const entityClass = entity.classList.contains('entity__contact') ? 'contact' :
+          entity.classList.contains('entity__company') ? 'company' : '';
 
       entity.querySelectorAll('[class*="entity__key__"]').forEach(item => {
         const key = item.className.split(' ').find(cls => cls.startsWith('entity__key__')).replace('entity__key__', '').toLowerCase();
@@ -98,10 +91,10 @@ const DogovorContainer = ({ type }) => {
 
   const checkAllFieldsFilled = (newEntities) => {
     const areCustomFieldsFilled = customFields.every(field => Object.keys(newEntities[field.entity]).length > 0);
-    
+
     let areFieldsFilled = false;
 
-    const contractListDocs = document.querySelector('.dogovor_fields__container');
+    const contractListDocs = document.querySelector('.clientrequest_fields__container');
     if (contractListDocs) {
       const fieldElements = contractListDocs.querySelectorAll('[class*="field__biba_id_"]');
       areFieldsFilled = Array.from(fieldElements).every(field => {
@@ -130,28 +123,38 @@ const DogovorContainer = ({ type }) => {
 
     const latestDoc = dataInfoDocs.docs.reduce((latest, current) => {
       return current.timestamp > latest.timestamp ? current : latest;
-    }, dataInfoDocs.docs[0]); // Добавим начальное значение для `reduce`, чтобы избежать ошибок, если массив пустой.
-    
+    }, dataInfoDocs.docs[0]);
+
     const latestName = latestDoc ? latestDoc.name : undefined;
 
     if (latestName !== undefined) {
-        const updateInputValue = (name, newValue) => {
-          const element = document.querySelector(`[data-id="${name}"]`);
-          if (element) {
-            const input = element.querySelector('input');
-            if (input) {
-              input.value = newValue;
-              input.dispatchEvent(new Event('input', { bubbles: true }));
-            }
+      const updateInputValue = (name, newValue) => {
+        const element = document.querySelector(`[data-id="${name}"]`);
+        if (element) {
+          const input = element.querySelector('input');
+          if (input) {
+            input.value = newValue;
+            input.dispatchEvent(new Event('input', { bubbles: true }));
           }
-        };
-        updateInputValue('1447475', latestName);
+        }
+      };
+      updateInputValue('1447475', latestName);
+    }
+  }
+
+  const loadDocuments = () => {
+    const inputElement = document.querySelector(`input[name="CFV[1277131]"]`);
+    if (inputElement) {
+      const savedData = inputElement.value;
+      setDocuments(savedData ? JSON.parse(savedData) : []);
+    } else {
+      setDocuments([]);
     }
   }
 
   useEffect(() => {
-
-    fetchDataInfo();
+    fetchDataInfo()
+    loadDocuments()
 
     const observer1 = new MutationObserver((mutationsList, observer) => {
       for (let mutation of mutationsList) {
@@ -205,70 +208,99 @@ const DogovorContainer = ({ type }) => {
   }, [fieldValues, entities]);
 
 
-  const handleGenerateDogovor = async () => {
+  const handleGenerateDogovor  = async () => {
+    const lead = document.querySelector('#lead_main_user-users_select_holder')
+    const lead_name = lead.querySelector('span').textContent
+    const managers = window.AMOCRM.constant("managers")
+    const matchedManager = Object.values(managers).find(manager => manager.title === lead_name)
 
-    let dataReal = null;
+    const lead_id = document.querySelector('#add_tags')
+    const id = Number(lead_id.querySelector('span').textContent.slice(1))
 
-    const dateElement = document.querySelector(`input[name="CFV[133081]"]`);
-    if (dateElement) {
-      let value = dateElement.value;
-      if (!value) {
-        dataReal = 'ignore'; 
-      } else {
-        dataReal = value;
-      }
-    }
+    const selected_bank = banks.find(bank => bank.name === fieldValues.bank.trim())
 
-    let dataName = null;
+    const client = document.querySelector('input[name="CFV[1276573]"]')
 
-    const nameElement = document.querySelector(`input[name="CFV[1447475]"]`);
-    if (nameElement) {
-      let value = nameElement.value;
-      if (!value) {
-        dataName = 'ignore'; 
-      } else {
-        dataName = value;
-      }
-    }
+    const rawValue = client.value;
+    const parsedValue = JSON.parse(rawValue);
 
-    let otherText = null;
-
-    const otherElement = document.querySelector(`input[name="CFV[1584463]"]`);
-    if (otherElement) {
-      let value = otherElement.value;
-      if (!value) {
-        otherText = 'ignore'; 
-      } else {
-        otherText = value;
-      }
-    }
-
-    setIsLoading(true); // установить состояние загрузки
-    const combinedData = {
-      entities,
-      fields: fieldValues,
-      summTotal: summTotal,
-      dataReal: dataReal,
-      otherText: otherText,
-      dataName: dataName
+    const requestBody = {
+      doc_type: "dogovor",
+      filename: "ДОГОВОР_ВИРТЭГ_для.docx",
+      amo_id: id,
+      phone: matchedManager.phone,
+      mail: matchedManager.login,
+      bank_adress: selected_bank.bank_adress,
+      biq: selected_bank.biq,
+      korr_bill: selected_bank.korr_bill,
+      raschetny_bill: selected_bank.raschetny_bill,
+      number_and_year: fieldValues.number_and_year,
+      post_adress: fieldValues.post_adress,
+      customer: parsedValue.name,
+      yur_adress: `${parsedValue.city} ${parsedValue.street} ${parsedValue.building} ${parsedValue.office}`,
+      customer_director_name: parsedValue.contactPerson,
+      customer_phone: parsedValue.company_phone,
+      customer_email: parsedValue.emailcompani,
+      customer_inn: parsedValue.inn,
+      customer_kpp: parsedValue.kpp,
+      customer_ogrn: parsedValue.ogrn,
+      customer_raschetny_bill: parsedValue.rs,
+      customer_bank: parsedValue.bank,
+      customer_komm_bill: parsedValue.ks,
+      customer_biq: parsedValue.bic
     };
-    console.log(combinedData);
-    
 
-    if (allFieldsFilled) {
-      const postData = async () => {
-        const postDataDocs = DocsLabels[type].createDocs;
-        await postDataDocs(combinedData);
-        await fetchDataInfo();
+    console.log(JSON.stringify(requestBody, null, 2))
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('https://24virteg.ru/api/generate-docx/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
-      await postData();
+
+      const responseData = await response.json();
+
+      await setDocuments(prev => Array.isArray(prev)
+          ? [...prev, `https://24virteg.ru/api/get-docx/?document_id=${responseData.document_id}&amo_id=${responseData.amo_id}`]
+          : [`https://24virteg.ru/api/get-docx/?document_id=${responseData.document_id}&amo_id=${responseData.amo_id}`]
+      );
+
+    } catch (error) {
+      console.error("Error:", error.message);
+    } finally {
+      setIsLoading(false);
     }
-    setIsLoading(false); // установить состояние загрузки
   };
 
+  useEffect(() => {
+    handleUpdateDocuments();
+  }, [documents]);
 
+  const handleUpdateDocuments = () => {
+    const inputElement = document.querySelector(`input[name="CFV[1277131]"]`)
+    if (inputElement) {
+      updateInputValue('1277131', JSON.stringify(documents || []));
+    }
+  }
 
-
+  const getEntityName = (entityType) => {
+    const targetInputName = entityType === 'contact' ? 'CFV[1276575]' : 'CFV[1276573]';
+    const targetInput = document.querySelector(`input[name="${targetInputName}"]`);
+    if (targetInput && targetInput.value) {
+      const entityData = JSON.parse(targetInput.value);
+      return entityData.name || 'данные отсутствуют';
+    }
+    return 'данные отсутствуют';
+  };
 
   return (
     <Container>
@@ -282,7 +314,7 @@ const DogovorContainer = ({ type }) => {
         style={{ padding: '5px 10px 10px 10px', borderRadius: '6px' }}
         className={DocsLabels[type].files}
       >
-        <FilesContainer dataDocs={dataDocs} />
+        <FilesContainer dataDocs={documents} name={'ДОГОВОР_ВИРТЭГ_для.docx'} />
       </Container>
 
       <CustomButton
@@ -300,35 +332,35 @@ const DogovorContainer = ({ type }) => {
       />
 
       <Container style={{ padding: '5px 10px 10px 10px', borderRadius: '6px' }}>
-        
-        <div style={{ marginTop: '5px', marginBottom: '5px' }}>
-        {customFields.map((field, index) => (
-          <ContainerHeader key={index} style={{ color: Object.keys(entities[field.entity]).length > 0 ? 'green' : 'red' }}>
-            <ContainerLeft style={{ flex: 4 }}>
-              {field.name}
-            </ContainerLeft>
-            <ContainerRight style={{ flex: 7, justifyContent: 'flex-start', paddingLeft: '8px' }}>
-              {Object.keys(entities[field.entity]).length > 0 ? entities[field.entity].name : 'данные отсутствуют'}
-            </ContainerRight>
-          </ContainerHeader>
-        ))}
+
+        <div style={{marginTop: '5px', marginBottom: '5px'}}>
+          {customFields.map((field, index) => (
+              <ContainerHeader key={index}
+                               style={{color: getEntityName(field.entity) !== 'данные отсутствуют' ? 'green' : 'red'}}>
+                <ContainerLeft style={{flex: 4}}>
+                  {field.name}
+                </ContainerLeft>
+                <ContainerRight style={{flex: 7, justifyContent: 'flex-start', paddingLeft: '8px'}}>
+                  {getEntityName(field.entity)}
+                </ContainerRight>
+              </ContainerHeader>
+          ))}
         </div>
-        
-        <div className="dogovor_fields__container">
+
+        <div className="clientrequest_fields__container">
           {fields.map(field => (
-            <ContainerHeader key={field.id}>
-              <CustomFields
-                id={field.id}
-                type={field.typeValue}
-                name={field.name}
-                nameId={field.nameId}
-                onFieldChange={handleFieldChange}
-              />
-            </ContainerHeader>
+              <ContainerHeader key={field.id}>
+                <CustomFields
+                    id={field.id}
+                    type={field.typeValue}
+                    name={field.name}
+                    nameId={field.nameId}
+                    onFieldChange={handleFieldChange}
+                />
+              </ContainerHeader>
           ))}
         </div>
       </Container>
-
     </Container>
   )
 };
